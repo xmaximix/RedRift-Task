555136deb7f4530ebf59029d51da06a08a3b6f6a
xmaximix <laruschely@gmail.com>
2022-12-16 17:44:49 +0500

    Minor polishing

--- Assets/Source/Scripts/Card/CardContainer.cs
+++ Assets/Source/Scripts/Card/CardContainer.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CardContainer : MonoBehaviour
 {
@@ -30,7 +32,7 @@ public class CardContainer : MonoBehaviour
         healthPoints = value;
         if (healthPoints < 1)
         {
-            RemoveFromHand();
+            StartCoroutine(Delay(RemoveFromHand, 1.2f));
         }
         cardView.UpdateHealthPoints(healthPoints);
     }
@@ -44,5 +46,11 @@ public class CardContainer : MonoBehaviour
     private void RemoveFromHand()
     {
         OnRemove?.Invoke(this);
+    }
+
+    private IEnumerator Delay(UnityAction callback, float time)
+    {
+        yield return new WaitForSeconds(time);
+        callback();
     }
 }
